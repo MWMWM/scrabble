41e8a5812179897b035ef090ca4aa0a834d37e7b
M <m@m.(none)>
2013-04-16 12:58:14 +0200

    reverse to amend

--- play/urls.py
+++ play/urls.py
@@ -1,7 +1,9 @@
 from django.conf.urls import patterns, url
+from play import views
 
 urlpatterns = patterns('play.views', 
-url(r'^$', 'Main', name = 'p_main'),
+url(r'^$', 'Main', {'letters': views.NewLetter()}, name = 'main'),
+url(r'^(?P<result>\d+)$', 'Main', {'letters': views.NewLetter()}, name = 'main2'),
+url(r'^(?P<result>\d+)\(?P<letters>(\w+)$', 'Main', name = 'main3'),
 url(r'^NewLetters$', 'NewLetters', name = 'newletters'),
-url(r'^create$', 'Create', name='click2'),
 )
--- play/views.py
+++ play/views.py
@@ -12,20 +12,26 @@ from django.core.urlresolvers import reverse
 from helper.models import Word, User
 from scrabble.views import RenderWithInf
 
-def Main(request, result=0):
-    letters = u'abć'
+def Main(request, letters, result=0):
     board = ''
     if 'check' in request.POST:
+        result = int(result)
         word = request.POST.get('word','')
-        result += len(word)
+        if word in letters:
+            result += AddPoints(word)
+            for letter in len(word):
+                string.replace(letters, letter, NewLetter())
+        return HttpResponseRedirect(reverse('play:main3', args=(letters, result )))
     return RenderWithInf('play/main.html', request, {
         'letters': letters, 'board': board, 'result':result})
 
 def NewLetters(request):
-    letters = random.choice(ascii_lowercase + u'ęóąśłżźćń')
-    print letters
-    return HttpResponseRedirect(reverse('p_main'))
+    letters = "".join(NewLetter() for i in range(6))
+    return  HttpResponseRedirect(reverse('play:main'))
 
-def Create(request):
-    if request.method =='POST':
-        return  HttpResponseRedirect(reverse('h_main'))
+def NewLetter():
+    letters = random.choice(ascii_lowercase) # + u'ęóąśłżźćń')
+    return letters
+
+def AddPoints(word):
+    return len(word)
