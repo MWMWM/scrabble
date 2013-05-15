8c3f4dab6e51dbd89272e55ee4b6406b232d173e
m <m@mw.(none)>
2013-05-15 19:26:10 +0200

    guessing word added, saving game without using js

--- play/views.py
+++ play/views.py
@@ -32,7 +32,11 @@ def Playing(request, result, all_letters, temp_letters):
             for letter in temp_letters:
                 all_letters = all_letters.replace(letter, NewLetter(language),1)
             w = Word.objects.get(word = temp_letters, language = language)
-            result += w.points 
+            result += w.points
+            player = UserProfile.objects.get(user = request.user)
+            if player.best_score < result:
+                player.best_score = result
+                player.save()
             return HttpResponseRedirect(reverse('playing', kwargs={
                 'all_letters': all_letters, 'temp_letters': "",
                 'result': result}))
@@ -50,23 +54,33 @@ def ChangeLetters(request, result, letters):
         else:
             messages.info(request, 'Koniec gry - uzyskano ujemną liczbę punktów')
             return HttpResponseRedirect(reverse('play'))
-        letters = "".join(NewLetter(language) for i in range(8))
+    letters = "".join(NewLetter(language) for i in range(8))
     return  HttpResponseRedirect(reverse('playing', 
         kwargs={'result': result, 'all_letters': letters, 'temp_letters': "" }))
 
-def Delete(request, result, all_letters, temp_letters, letter=''):
+def Delete(request, where, temp_letters, letter=''):
     temp_letters = temp_letters.replace(letter, '', 1)
-    return HttpResponseRedirect(reverse('playing', kwargs={'result':result, 
-        'all_letters': all_letters, 'temp_letters': temp_letters}))
+    return HttpResponseRedirect('/' + where + '/' + temp_letters)
 
-def Save(request, score):
-    player = UserProfile.objects.get(user = request.user)
-    score = int(score)
-    if player.best_score < score:
-        player.best_score = score
-        player.save()
-    print player.best_score
-    return HttpResponse()
+def Guess(request, result=0, guesses=0, all_letters='', temp_letters=''):
+    left_letters = list((Counter(all_letters) - Counter(temp_letters)).elements())
+    if not left_letters:
+        if all_letters:
+            language = request.session.get('language', 'pl')
+            language = Language.objects.get(short=language)
+            guesses = int(guesses) + 1
+            if Word.objects.filter(word = temp_letters, language = language).exists():
+                result = int(result) + 1
+                messages.info(request, "Utworzony wyraz był poprawny")
+            else:
+                word = Word.objects.filter(word = all_letters, language = language)[0]
+                messages.info(request, u"Utworzony wyraz nie był poprawny, \
+                        można było utworzyć <{}>".format(word))
+        all_letters = random.choice(Word.objects.all())
+        return HttpResponseRedirect(reverse('guessed', kwargs={
+            'result': result, 'guesses': guesses, 'all_letters': all_letters}))
+    return RenderWithInf('play/guess.html', request, {'result': result, 
+        'guesses': guesses, 'temp_letters': temp_letters, 'left_letters': left_letters})
 
 def NewLetter(language):
     return random.choice(language.letters)
